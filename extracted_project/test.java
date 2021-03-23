7ac65673cb16651e785568320c30d25ac59bced3
rrnr95 <rrnr95@users.noreply.github.com>
2021-03-23 14:20:14 +0000

    +Extractor +extracted_project

---
+++ extracted_project/test.java
@@ -0,0 +1,11 @@
+public class test {
+	public static void main(String[] args) {
+	}
+	public void get() {
+		return;
+	}
+	private int get2() {
+		if(true)
+			return 0;
+	}
+}
---
+++ src/main/java/Extractor.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public final class Extractor {
+
+	private final static File EXTRACTION_FOLDER = new File("extracted_project");
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+
+		ArrayList<String> cls_path_list = new ArrayList<String>();
+		cls_path_list = getAllClassNames(EXTRACTION_FOLDER, cls_path_list);
+
+		for(String path: cls_path_list) {
+			int num_lines = LineCounter.getNumberOfLines(new BufferedReader(new FileReader(path)));
+			System.out.println(num_lines);
+		}
+
+	}
+
+	//Vai buscar todos os ficheiros ".java" dentro de uma pasta,
+	//caso encontre subpastas, chama o método recursivamente nessas subpastas.
+	public static ArrayList<String> getAllClassNames(File root, ArrayList<String> list) {
+
+		File[] file_list = root.listFiles();
+		if(file_list == null)
+			throw new IllegalStateException("A diretoria não contém ficheiros"); 
+
+		for (File file : file_list) {
+
+			if (file.isDirectory())
+				getAllClassNames(file, list); //chamada recursiva
+
+			else {
+				final String file_path = file.getAbsolutePath();
+
+				if(file_path.endsWith(".java")) {
+					list.add(file_path);
+					System.out.println("Added ->  " + file_path);
+				}
+			}
+		}
+
+		if(list.isEmpty()) 
+			throw new IllegalStateException("Lista de classes vazia");
+
+		return list;
+	}
+}
