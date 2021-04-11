11692ef713a64bc95490cc83e0474b3bc7ef3691
rrnr95 <rrnr95@users.noreply.github.com>
2021-04-11 20:09:39 +0100

    +NumberOfClassesPerFileTest

--- src/main/java/tests/NumberOfClassesPerFileTest.java
+++ src/main/java/tests/NumberOfClassesPerFileTest.java
@@ -2,13 +2,30 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
-public class NumberOfClassesPerFileTest {
+import backend.NumberOfClassesPerFile;
 
+public class NumberOfClassesPerFileTest {
+	
+	private static final String FILE = "C:\\Users\\renat\\Documents\\GitHub_repository\\ES-2Sem-2021-Grupo12\\imported_project\\not_source\\src\\pckg2\\HelloWorld.java";
+	private final NumberOfClassesPerFile noc= new NumberOfClassesPerFile(FILE);
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals(NumberOfClassesPerFile.class , noc.getClass());
+		
+		List<String> classes = noc.getClasses();
+		
+		List<String> expected_classes = new ArrayList<String>();
+		
+		expected_classes.add("HelloWorld");
+		expected_classes.add("Wtv");
+		
+		assertEquals(expected_classes, classes);
 	}
 
 }
