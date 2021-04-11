d2e93ffe70a38370acb9060b46f8164545435a96
rrnr95 <rrnr95@users.noreply.github.com>
2021-04-11 18:35:27 +0100

    +PackageCounterTest

--- src/main/java/tests/PackageCounterTest.java
+++ src/main/java/tests/PackageCounterTest.java
@@ -2,13 +2,41 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
-public class PackageCounterTest {
+import backend.PackageCounter;
 
+public class PackageCounterTest {
+	
+	private static final String PATH = "C:\\Users\\renat\\Documents\\GitHub_repository\\ES-2Sem-2021-Grupo12\\imported_project";
+	private static final String PATH_2 = "C:\\Users\\renat\\Documents\\GitHub_repository\\ES-2Sem-2021-Grupo12\\imported_project\\not_source";
+	private static final PackageCounter pc = new PackageCounter(PATH);
+	private static final PackageCounter pc_2 = new PackageCounter(PATH_2);
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		assertEquals(PackageCounter.class, pc.getClass());
+		
+		//IMPORTED_PROJECT#1
+		int num_packages = pc.packagesCount();	
+		assertEquals(3, num_packages);
+		
+		List<String> pack_list = pc.getPackages();
+		List<String> expected_pck_list = new ArrayList<String>();
+		
+		expected_pck_list.add("pckg2");
+		expected_pck_list.add("pckg2.2");
+		expected_pck_list.add("pckg");
+		
+		assertEquals(expected_pck_list, pack_list);
+		
+		int num_packages_2 = pc_2.packagesCount();	
+		assertEquals(3, num_packages_2);
+		
 	}
 
 }
