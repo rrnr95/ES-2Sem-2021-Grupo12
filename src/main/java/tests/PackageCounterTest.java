32189c84a4c2d4df623bc6a056c14ee0cc406b4d
rrnr95 <rrnr95@users.noreply.github.com>
2021-04-11 16:43:59 +0100

    +esqueleto do package tests

---
+++ src/main/java/tests/PackageCounterTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PackageCounterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
