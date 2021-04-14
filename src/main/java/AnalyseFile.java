3627fe2a5b500746842d77e349e3749c30bec048
erikaevora <erika.evora@gmail.com>
2021-04-14 11:50:17 +0100

    Update AnalyseFile.java

--- src/main/java/AnalyseFile.java
+++ src/main/java/AnalyseFile.java
@@ -1,3 +1,12 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import backend.CycloMethod;
+import backend.LineCounter;
+import backend.MethodUtils;
+import backend.NumberOfClassesPerFile;
+
 public class AnalyseFile extends Thread {
 	private String parentPackage;
 	private String pathToFile;
@@ -10,18 +19,35 @@ public class AnalyseFile extends Thread {
 	}
 	
 	public void run() {
-		//1 - encontrar cada classe dentro do ficheiro .java;		
+
+		//1 - encontrar cada classe dentro do ficheiro .java;	
+		NumberOfClassesPerFile noc = new NumberOfClassesPerFile(pathToFile);
+		final List<String> classes = noc.getClasses();
+		
+		LineCounter ln = new LineCounter(pathToFile);
 		
 		//2 - para cada classe, levantamento dos metodos;
-		//for (metodo m : class.getMetodos()) {
+		List<String> methodCode = new MethodUtils(pathToFile).getMethodCode();
+		final List<Integer> cyclo_method_list = CycloMethod.allMethodsCycloValue(methodCode);
+		
+		final Map<String, Integer> loc_method_hash = ln.getMethodNameLines();
 		
 		//		3 - para cada metodo, criar um Objeto MethodStats e colocar no RecursoPartilhado
-		//}
-		
+		int i = 0;
+		for (Map.Entry<String, Integer> m : loc_method_hash.entrySet()) {
+			MethodStats ms = new MethodStats(i+1, parentPackage, pathToFile, classes, m.getKey(), m.getValue(), 0, 0, ln.getLinesCount(), 0);
+			
+			ms.setNOM_class(methodCode.size());
+			ms.setCYCLO_method(cyclo_method_list.indexOf(i));
+			ms.setWMC_class(CycloMethod.wmcCalculator(cyclo_method_list));
+			
+			metodos.addMetodo(ms);
+			i++;
+		}
 		
 		//exemplo:
-		MethodStats ms = new MethodStats(0, parentPackage, pathToFile, null, "metodo", 0, 0, 0, 10, 10);
-		metodos.addMetodo(ms);
+//		MethodStats ms = new MethodStats(0, parentPackage, pathToFile, null, "metodo", 0, 0, 0, 10, 10);
+//		metodos.addMetodo(ms);
 	
 	}
 
