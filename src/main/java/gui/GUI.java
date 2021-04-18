b314921222a7341264dfa4b7af9468b5f6abdc5c
erikaevora <erika.evora@gmail.com>
2021-04-18 18:21:07 +0100

    Update GUI.java
    importacao do excel ainda nao funciona

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -3,20 +3,38 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import extractor.CodeSmells;
+
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.ScrollPane;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+
 
 public class GUI {
 
 	private static JFrame frmExtractMetrics;
 	private static JTextField txtf_path;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -49,7 +67,7 @@ public class GUI {
 		frmExtractMetrics.setBackground(Color.GRAY);
 		frmExtractMetrics.setTitle("Extract metrics");
 		frmExtractMetrics.setResizable(false);
-		frmExtractMetrics.setBounds(100, 100, 395, 355);
+		frmExtractMetrics.setBounds(100, 100, 928, 620);
 		frmExtractMetrics.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JPanel panel = new JPanel();
@@ -59,17 +77,34 @@ public class GUI {
 
 		txtf_path = new JTextField();
 		txtf_path.setEditable(false);
-		txtf_path.setBounds(40, 40, 232, 20);
+		txtf_path.setBounds(10, 541, 232, 20);
 		panel.add(txtf_path);
 		txtf_path.setColumns(10);
 
 		JButton btn_folder = new JButton("Folder");
 		addChooseListener(btn_folder);
 
-		btn_folder.setBounds(282, 39, 74, 23);
+		btn_folder.setBounds(250, 540, 74, 23);
 		panel.add(btn_folder);
+		
+		JButton btnCalculateMetrics = new JButton("Calculate Metrics");
+		btnCalculateMetrics.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				calculateMetricsPressed();
+			}
+		});
+		btnCalculateMetrics.setBounds(772, 540, 140, 23);
+		panel.add(btnCalculateMetrics);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 902, 519);
+		panel.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
 	}
 
+	
 	private static void addChooseListener(JButton btn) {
 		btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -91,4 +126,90 @@ public class GUI {
 		});
 	}
 	
+	
+	private void calculateMetricsPressed() {
+		
+		File project = new File(txtf_path.getText());
+		if (project.exists()) {
+			
+			CodeSmells cs = new CodeSmells(project.getAbsolutePath());
+			try {
+				cs.init();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				JOptionPane.showMessageDialog(null, e.getMessage());
+			}
+			
+			try {
+				Thread.sleep(3000);
+				printMetricsGUI(project);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} else {
+			
+			JOptionPane.showMessageDialog(null,"Diretoria Desconhecida");
+		}
+		
+//		printMetricsGUI(new File("C:\\Users\\Utilizador\\eclipse-workspace\\BattleshipCodeCoverage-master\\Battleship"));
+	}
+	
+	
+	private void printMetricsGUI(File project) {
+		
+		FileInputStream excelFIS = null;
+		BufferedInputStream excelBIS = null;
+        XSSFWorkbook excelImportToJTable = null;
+        DefaultTableModel model = new DefaultTableModel();
+		try {
+            File excelFile = new File(project.getAbsolutePath() + "\\smells.xlsx");
+            excelFIS = new FileInputStream(excelFile);
+            excelBIS = new BufferedInputStream(excelFIS);
+            excelImportToJTable = new XSSFWorkbook(excelBIS);
+            XSSFSheet excelSheet = excelImportToJTable.getSheetAt(0);
+
+            for (int row = 0; row < excelSheet.getLastRowNum(); row++) {
+                XSSFRow excelRow = excelSheet.getRow(row);
+
+                XSSFCell excelMethod_id = excelRow.getCell(0);
+                System.out.println(excelMethod_id);
+                XSSFCell excelPackage = excelRow.getCell(1);
+                XSSFCell excelClass = excelRow.getCell(2);
+                XSSFCell excelInner_classes = excelRow.getCell(3);
+                XSSFCell excelMethod = excelRow.getCell(4);
+                XSSFCell excelNOM_class = excelRow.getCell(5);
+                XSSFCell excelLOC_class = excelRow.getCell(6);
+                XSSFCell excelWMC_class = excelRow.getCell(7);
+                XSSFCell excelIs_God_class = excelRow.getCell(8);
+                XSSFCell excelLOC_method = excelRow.getCell(9);
+                XSSFCell excelCYCLO_method = excelRow.getCell(10);
+                XSSFCell excelIs_long_method = excelRow.getCell(11);
+
+                model.addRow(new Object[]{excelMethod_id, excelPackage, excelClass, excelInner_classes, excelMethod, excelNOM_class, excelLOC_class, excelWMC_class, excelIs_God_class, excelLOC_method, excelCYCLO_method, excelIs_long_method});
+            }
+            table.setModel(model);
+            
+           // JOptionPane.showMessageDialog(null, "Imported Successfully !!.....");
+        } catch (IOException iOException) {
+            JOptionPane.showMessageDialog(null, iOException.getMessage());
+        } finally {
+            try {
+                if (excelFIS != null) {
+                    excelFIS.close();
+                }
+                if (excelBIS != null) {
+                    excelBIS.close();
+                }
+                if (excelImportToJTable != null) {
+                    excelImportToJTable.close();
+                }
+            } catch (IOException iOException) {
+                JOptionPane.showMessageDialog(null, iOException.getMessage());
+            }
+        }
+		table.revalidate();
+        table.repaint();
+	}
 }
