fd65acbec5657f7a2db9a36abd41ffaa1bc06835
rrnr95 <rrnr95@users.noreply.github.com>
2021-05-04 17:53:58 +0100

    WriteToXLSX.java javadoc updated

--- src/main/java/extractor/WriteToXLSX.java
+++ src/main/java/extractor/WriteToXLSX.java
@@ -1,85 +1,5 @@
-//<<<<<<< HEAD:src/main/java/extractor/WriteToXLSX.java
 package extractor;
-//import java.io.FileOutputStream;
-//import org.apache.poi.ss.usermodel.Cell;
-//import org.apache.poi.ss.usermodel.CellStyle;
-//import org.apache.poi.ss.usermodel.Font;
-//import org.apache.poi.ss.usermodel.Row;
-//import org.apache.poi.ss.usermodel.Sheet;
-//import org.apache.poi.ss.usermodel.Workbook;
-//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-//
-//public class WriteToXLSX {
-//	private Sheet sh;
-//	private Workbook wb;
-//	private String path;
-//	private RecursoPartilhado rec;
-//	private int numOfCol;
-//	
-//	public WriteToXLSX(String path, RecursoPartilhado rec) {
-//		this.path = path;
-//		this.wb = new XSSFWorkbook();
-//		this.sh = wb.createSheet("code_smells");
-//		this.rec = rec;
-//	}
-//	
-//	public void init() {
-//		createHeader();
-//		populate();
-//		resizeCell();
-//		write();
-//	}
-//	
-//	private void createHeader() {
-//		String[] columns = {"Method Id", "package", "class", "inner classes", "method", "NOM_class", "LOC_class", "WMC_class", "is_God_class", "LOC_method", "CYCLO_method", "is_long_method"};
-//		numOfCol = columns.length;
-//		
-//		Font headerFont = wb.createFont();
-//		headerFont.setBold(true);
-//		headerFont.setFontHeightInPoints((short)12);
-//		
-//		CellStyle headerStyle = wb.createCellStyle();
-//		headerStyle.setFont(headerFont);
-//		
-//		Row headerRow = sh.createRow(0);
-//		for (int i = 0; i < numOfCol; i++) {
-//			Cell cell = headerRow.createCell(i);
-//			cell.setCellValue(columns[i]);
-//			cell.setCellStyle(headerStyle);
-//		}
-//	}
-//	
-//	private void populate() {
-//		int l = 1;
-//		for(MethodStats stat : rec.getMethodStats()) {
-//			Row row = sh.createRow(l++);
-//			int c = 0;
-//			for (String s : stat.getMethodAsList()) {
-//				Cell cell = row.createCell(c++);
-//				cell.setCellValue(s);
-//			}
-//		}
-//	}
-//	
-//	private void resizeCell() {
-//		for (int i = 0; i < numOfCol; i++) {
-//			sh.autoSizeColumn(i);
-//		}
-//	}
-//	
-//	private void write() {
-//		try {			
-//			FileOutputStream out = new FileOutputStream(path);
-//			wb.write(out);
-//			out.close();
-//		}
-//		catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//
-//
-//=======
+
 import java.io.FileOutputStream;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
@@ -89,7 +9,12 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-//TODO Rename ExcelUtils ?
+
+/**
+ * 		Class used to create and write to a .xlsx file 
+ * 		@author ES-2Sem-2021-Grupo12
+ */
+
 public class WriteToXLSX {
 	
 	static String[] columns = {	"Method Id", 
@@ -105,13 +30,12 @@ public class WriteToXLSX {
 								"CYCLO_method", 
 								"is_long_method"};
 	static int numOfCol = columns.length;
-	
-//	static private int numOfCol;
+
 	
 	/**
 	 * 			Constructor
 	 * @param 	path
-	 * 			path location where to save file
+	 * 			Location(String) where the file is to be saved
 	 * @param 	rec
 	 * 		 	Metrics list
 	 */
@@ -126,7 +50,11 @@ public class WriteToXLSX {
 	
 	
 	/**
-	 * 	Create header excel header
+	 * 			Creates an excel header
+	 * 	@param 	wb
+	 * 			Workbook that contains the sheet 
+	 * 	@param	sh
+	 * 			Sheet where the header is to be created
 	 */
 	static private void createHeader( Workbook wb, Sheet sh ) {
 		
@@ -146,8 +74,12 @@ public class WriteToXLSX {
 	}
 	
 	/**
-	 * Populate the excel file
-	 * It uses a MethodStats as a list for each row, iterates over it and write each value to a cell
+	 * 			Populate the excel file
+	 * 			It uses a MethodStats as a list for each row, iterates over it and writes each value to a cell
+	 * 	@param	rec
+	 * 			Shared Resource with the MethodStats list
+	 * 	@param 	sh
+	 * 			Sheet where the list information is to be dumped
 	 */
 	static private void populate(RecursoPartilhado rec, Sheet sh) {
 		int index = 1;
@@ -168,8 +100,11 @@ public class WriteToXLSX {
 		}
 	}
 	
+	
 	/**
-	 * Resize all cells
+	 * 			Resize all cells
+	 * 	@param	sh
+	 * 			Sheet in which the resizing is to be applied
 	 */
 	static private void resizeCell(Sheet sh) {
 		for (int i = 0; i < numOfCol; i++) {
@@ -178,7 +113,11 @@ public class WriteToXLSX {
 	}
 	
 	/**
-	 * Write the XSSFWorkbook to file
+	 * 			Write the XSSFWorkbook to file
+	 * 	@param	path
+	 *			Location for the file
+	 * 	@param	wb
+	 * 			Workbook to be copied
 	 */
 	static private void write(String path, Workbook wb) {
 		try {			
@@ -190,6 +129,4 @@ public class WriteToXLSX {
 			e.printStackTrace();
 		}
 	}
-
-//>>>>>>> refactor:src/main/java/WriteToXLSX.java
 }
